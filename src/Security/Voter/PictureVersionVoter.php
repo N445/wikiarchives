06f0f8bdfe7c735fa9f237dd412d62f413ddbd16
Robin PARISOT <parisrob@hotmail.fr>
2021-11-02 19:31:43 +0000

    add is verify checker

--- src/Security/Voter/PictureVersionVoter.php
+++ src/Security/Voter/PictureVersionVoter.php
@@ -1,7 +1,8 @@
 <?php
     
     namespace App\Security\Voter;
-    
+
+    use App\Entity\User;
     use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
     use Symfony\Component\Security\Core\Authorization\Voter\Voter;
     use Symfony\Component\Security\Core\User\UserInterface;
@@ -9,7 +10,7 @@
     class PictureVersionVoter extends Voter
     {
         public const PICTURE_VERSION_CREATE = 'PICTURE_VERSION_CREATE';
-        
+    
         protected function supports(string $attribute, $subject): bool
         {
             // replace with your own logic
@@ -17,21 +18,21 @@
             return in_array($attribute, [self::PICTURE_VERSION_CREATE])
                 && $subject instanceof UserInterface;
         }
-        
+    
         protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
         {
-    
+        
             /** @var User $user */
             $user = $token->getUser();
             // if the user is anonymous, do not grant access
-            
+        
             if (!$user instanceof UserInterface) {
                 return false;
             }
             // ... (check conditions and return true to grant permission) ...
             switch ($attribute) {
                 case self::PICTURE_VERSION_CREATE:
-                    return $user->getRights()->getHasVersionCreator();
+                    return $user->getRights()->getHasVersionCreator() && $user->isVerified();
             }
             return false;
         }

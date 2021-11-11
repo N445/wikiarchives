3a6ca7538d1430d16e317588bd739f49d2e0491c
Robin PARISOT <parisrob@hotmail.fr>
2021-11-11 12:24:06 +0000

    edit map layer

---
+++ public/build/admin-catalog-datatable-js.js
@@ -0,0 +1,25 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["admin-catalog-datatable-js"],{
+
+/***/ "./assets/admin/catalog/catalog-datatable.js":
+/*!***************************************************!*\
+  !*** ./assets/admin/catalog/catalog-datatable.js ***!
+  \***************************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+/* provided dependency */ var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+__webpack_require__(/*! datatables.net-bs5 */ "./node_modules/datatables.net-bs5/js/dataTables.bootstrap5.js");
+
+$(document).ready(function () {
+  $('.datatable').DataTable();
+});
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, ["vendors-node_modules_jquery_dist_jquery_js","vendors-node_modules_datatables_net-bs5_js_dataTables_bootstrap5_js"], () => (__webpack_exec__("./assets/admin/catalog/catalog-datatable.js")));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYWRtaW4tY2F0YWxvZy1kYXRhdGFibGUtanMuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7O0FBQUFBLG1CQUFPLENBQUUseUZBQUYsQ0FBUDs7QUFFQUMsQ0FBQyxDQUFDQyxRQUFELENBQUQsQ0FBWUMsS0FBWixDQUFrQixZQUFXO0FBQ3pCRixFQUFBQSxDQUFDLENBQUMsWUFBRCxDQUFELENBQWdCRyxTQUFoQjtBQUNILENBRkQiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hc3NldHMvYWRtaW4vY2F0YWxvZy9jYXRhbG9nLWRhdGF0YWJsZS5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJyZXF1aXJlKCAnZGF0YXRhYmxlcy5uZXQtYnM1JyApO1xuXG4kKGRvY3VtZW50KS5yZWFkeShmdW5jdGlvbigpIHtcbiAgICAkKCcuZGF0YXRhYmxlJykuRGF0YVRhYmxlKCk7XG59ICk7Il0sIm5hbWVzIjpbInJlcXVpcmUiLCIkIiwiZG9jdW1lbnQiLCJyZWFkeSIsIkRhdGFUYWJsZSJdLCJzb3VyY2VSb290IjoiIn0=
